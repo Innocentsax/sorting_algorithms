c048563a381d038259021d14c8dd62d3976a832a
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-04-23 20:19:14 +0100

    Sorting Algorithm and Big O

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -16,7 +16,7 @@ void merge_sort(int *array, size_t size);
 void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
 		size_t back)
 {
-	size_t a, b, k = 0;
+	size_t i, j, k = 0;
 
 	printf("Merging...\n[left]: ");
 	print_array(subarr + front, mid - front);
@@ -24,14 +24,14 @@ void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
 	printf("[right]: ");
 	print_array(subarr + mid, back - mid);
 
-	for (a = front, b = mid; a < mid && b < back; k++)
-		buff[k] = (subarr[a] < subarr[b]) ? subarr[i++] : subarr[b++];
-	for (; a < mid; a++)
-		buff[k++] = subarr[a];
-	for (; b < back; b++)
-		buff[k++] = subarr[b];
-	for (a = front, k = 0; a < back; a++)
-		subarr[a] = buff[k++];
+	for (i = front, j = mid; i < mid && j < back; k++)
+		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
+	for (; i < mid; i++)
+		buff[k++] = subarr[i];
+	for (; j < back; j++)
+		buff[k++] = subarr[j];
+	for (i = front, k = 0; i < back; i++)
+		subarr[i] = buff[k++];
 
 	printf("[Done]: ");
 	print_array(subarr + front, back - front);
